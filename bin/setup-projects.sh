20cf1498aa2f38ff5eefb697fd149f6415b64955
Attila Mezei-Horvati <AMezei-Horvati@web.com>
2017-11-10 14:34:53 -0500

    we need an installer for the other projects

---
+++ bin/setup-projects.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# check out all other repo-s
+
+# front-end
+mkdir -p ../apps/frontend
+git clone https://github.com/attilamh/frontend.git ../apps/frontend
+
+mkdir -p ../apps/backend
+git clone https://github.com/attilamh/frontend.git ../apps/backend
