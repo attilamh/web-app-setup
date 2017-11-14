541ac3782e9a1d39de22f429f0748230d9e1338f
Attila Mezei-Horvati <AMezei-Horvati@web.com>
2017-11-14 17:29:30 -0500

    Fixed docker install by copying package.json over before image is created so npm install will work.

--- bin/setup-projects.sh
+++ bin/setup-projects.sh
@@ -1,14 +1,16 @@
 #/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# check out all other repo-s
+# check out all other repo-s and setup docker compose
 
-# front-end
+# checkout front-end
 mkdir -p ../apps/frontend
 git clone https://github.com/attilamh/frontend.git ../apps/frontend
+# copy package.json to fronted docker so install can succeed
+cp ../apps/frontend/package.json ../setup/frontend
 
 mkdir -p ../apps/backend
 git clone https://github.com/attilamh/frontend.git ../apps/backend
 
 cd ../setup
-docker-compose up
+docker-compose up --build
