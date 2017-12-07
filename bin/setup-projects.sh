49b7c5a21bd45857f428a47e6f582f14c7b376a7
Attila Mezei-Horvati <AMezei-Horvati@web.com>
2017-12-07 15:01:24 -0500

    reenable backend

--- bin/setup-projects.sh
+++ bin/setup-projects.sh
@@ -1,17 +1,19 @@
 #/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+mkdir -p ../libs/maven ../libs/node_modules
+
 # check out all other repo-s and setup docker compose
 
-# checkout front-end
+# checkout frontend
 mkdir -p ../apps/frontend
 git clone https://github.com/attilamh/frontend.git ../apps/frontend
 # copy package.json to fronted docker so install can succeed
 cp ../apps/frontend/package.json ../setup/frontend
 
-# TODO
-#mkdir -p ../apps/backend
-#git clone https://github.com/attilamh/backend.git ../apps/backend
+# checkout backend
+mkdir -p ../apps/backend
+git clone https://github.com/attilamh/backend.git ../apps/backend
 
 cd ../setup
 docker-compose build --no-cache
